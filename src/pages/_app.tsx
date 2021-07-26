c0b1c4041b6e68210ba310297b3d0a6a3c8c517f
Fischer <fischerrobson@gmail.com>
2021-07-26 13:30:50 -0300

    next auth com github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { Provider as NextAuthProvier } from "next-auth/client";
+
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvier session={pageProps.sesion}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvier>
   )
 }
 
--- src/pages/api/users/[id].ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next"
-
-export function users(request: NextApiRequest, response: NextApiResponse) {
-  console.log(request.query);
-  //retorna o valor passado em users/id
-}
