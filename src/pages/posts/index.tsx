97625fe44948d237a008056663d82b0e7bf3f689
Fischer <fischerrobson@gmail.com>
2021-08-01 23:30:53 -0300

    consumindo a api do prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,7 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
 import styles from "./styles.module.scss";
 
 export default function Posts() {
@@ -30,4 +33,24 @@ export default function Posts() {
 
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const respose = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['plublication.title', 'publication.content'],
+    pageSize: 100
+  });
+
+  console.log(respose)
+
+  return {
+    props: {
+
+    }
+  }
+
 }
