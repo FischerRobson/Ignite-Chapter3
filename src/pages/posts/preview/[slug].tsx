cc13a649117fe6a2f6e25b27dbd37b01e0bc0409
Fischer <fischerrobson@gmail.com>
2021-08-03 11:56:07 -0300

    fim do capitulo 3

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/client";
 import { useRouter } from "next/dist/client/router";
 import Head from "next/head";
@@ -56,9 +56,15 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = () => {
   return {
-    paths: [],
+    paths: [
+      {
+        params: {
+          slug: ""
+        }
+      }
+    ],
     fallback: "blocking",
   }
 }
@@ -86,7 +92,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post
-    }
+    },
+    revalidate: 60 * 30, // 30 minutos
   }
 
 }
