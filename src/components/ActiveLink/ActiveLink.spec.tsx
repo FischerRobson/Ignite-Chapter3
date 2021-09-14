66296e70a7cdc562076e3a79b1d93ebada26e828
Fischer <fischerrobson@gmail.com>
2021-09-14 13:35:04 -0300

    teste do active link

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,37 @@
+import { render } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/dist/client/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      }
+    }
+  }
+}); //mock do useRouter do Next
+
+describe("ActiveLinkComponent", () => {
+  test("active link render correctly", () => {
+    const { debug, getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    debug();
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  test("active link is receiving active class", () => {
+    const { debug, getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    debug();
+    expect(getByText("Home")).toHaveClass("active");
+  });
+});
+
